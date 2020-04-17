c26e7f8012688dc94cc0674e07eb7bfbd76e1036
Maria Zinkovskaya <my.project.git@gmail.com>
2020-04-17 23:39:34 +0300

    Added task-5

---
+++ js/task-5.js
@@ -0,0 +1,8 @@
+let question = prompt('Введите страну назначения');
+question = question.toLowerCase();
+
+switch (question) {
+  case 'китай':
+    console.log(`Доставка в Китай будет стоить 100 кредитов`);
+    break;
+}
